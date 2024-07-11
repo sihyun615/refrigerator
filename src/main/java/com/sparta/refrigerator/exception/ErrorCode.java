4fa55bb97352fa735ac110832482db051ecd1303
leesewon <leesw1945@gmail.com>
2024-07-12 03:22:12 +0900

    🎉 add : #4 UserException 추가

---
+++ src/main/java/com/sparta/refrigerator/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.sparta.refrigerator.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+    DUPLICATE_UESR(400, "중복된 사용자가 존재합니다."),
+    INCORRECT_ADMIN(400, "관리자 암호가 일치하지 않습니다."),
+    USER_NOT_FOUND(400, "해당 아이디의 유저를 찾지 못했습니다.");
+
+    private int status;
+    private String msg;
+}
