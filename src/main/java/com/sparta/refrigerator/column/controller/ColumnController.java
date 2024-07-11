3aaa6381bd56ce0b7b3baf758a6d12f895937da9
hongsy521 <seoyeong0521@naver.com>
2024-07-12 02:02:20 +0900

    ♻️ refactor : Controller ResponseEntity로 감싸기

--- src/main/java/com/sparta/refrigerator/column/controller/ColumnController.java
+++ src/main/java/com/sparta/refrigerator/column/controller/ColumnController.java
@@ -7,6 +7,8 @@ import com.sparta.refrigerator.column.service.ColumnService;
 import com.sparta.refrigerator.common.response.DataCommonResponse;
 import com.sparta.refrigerator.common.response.StatusCommonResponse;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,34 +20,38 @@ public class ColumnController {
     private ColumnService columnService;
 
     @PostMapping("/admin/boards/{boardId}/columns")
-    public StatusCommonResponse addColumn(@PathVariable(name = "boardId") Long boardId,
+    public ResponseEntity<StatusCommonResponse> addColumn(@PathVariable(name = "boardId") Long boardId,
         @RequestBody ColumnRequestDto requestDto,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
         columnService.addColumn(boardId, requestDto, userDetails.getUser());
-        return new StatusCommonResponse(201, "컬럼 추가되었습니다.");
+        StatusCommonResponse response = new StatusCommonResponse(201, "컬럼 추가되었습니다.");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/admin/columns/{columnId}")
-    public StatusCommonResponse deleteColumn(@PathVariable(name = "columnId") Long columnId,
+    public ResponseEntity<StatusCommonResponse> deleteColumn(@PathVariable(name = "columnId") Long columnId,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
         columnService.deleteColumn(columnId, userDetails.getUser());
-        return new StatusCommonResponse(204, "컬럼 삭제되었습니다.");
+        StatusCommonResponse response = new StatusCommonResponse(204, "컬럼 삭제되었습니다.");
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
     @GetMapping("/boards/{boardId}/columns")
-    public DataCommonResponse<List<ColumnResponseDto>> getAllColumns(
+    public ResponseEntity<DataCommonResponse<List<ColumnResponseDto>>> getAllColumns(
         @PathVariable(name = "boardId") Long boardId) {
         List<ColumnResponseDto> columns = columnService.getAllColumnsOrderByIndex(boardId);
-        return new DataCommonResponse<>(200, "컬럼 전체 조회 성공하였습니다.", columns);
+        DataCommonResponse<List<ColumnResponseDto>> response = new DataCommonResponse<>(200, "컬럼 전체 조회 성공하였습니다.", columns);
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
 
     @PutMapping("/admin/boards/{boardId}/columns/{columnId}/transfer")
-    public StatusCommonResponse moveColumn(@PathVariable(name = "boardId") Long boardId,
+    public ResponseEntity<StatusCommonResponse> moveColumn(@PathVariable(name = "boardId") Long boardId,
         @PathVariable(name = "columnId") Long columnId,
         @RequestBody ColumnMoveRequestDto requestDto,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
         columnService.moveColumn(boardId, columnId, requestDto, userDetails.getUser());
-        return new StatusCommonResponse(200, "컬럼 이동되었습니다.");
+        StatusCommonResponse response = new StatusCommonResponse(200, "컬럼 이동되었습니다.");
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
 }
