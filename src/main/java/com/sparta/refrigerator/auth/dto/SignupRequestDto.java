e63c2e7660a01829dda5fa3ac8c8c178e95382d1
leesewon <leesw1945@gmail.com>
2024-07-12 23:37:22 +0900

    ✨ fix : #25 User Frontend 연동

--- src/main/java/com/sparta/refrigerator/auth/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/refrigerator/auth/dto/SignupRequestDto.java
@@ -2,13 +2,11 @@ package com.sparta.refrigerator.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class SignupRequestDto {
     @NotBlank
     @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9]).{4,10}",
@@ -20,7 +18,7 @@ public class SignupRequestDto {
         message = "비밀번호는 최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자로 이루어져 있어야 합니다.")
     private String password;
 
-    private String adminToken;
+    private String adminToken = "";
 
-    private boolean admin;
+    private boolean admin = false;
 }
