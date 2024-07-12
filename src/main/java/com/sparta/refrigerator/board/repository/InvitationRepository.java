45383b140bdbfc8690e16dfd4f16c0cdc7700e66
hoon <dotofi@naver.com>
2024-07-12 16:52:16 +0900

    🩹 fix : #8 초대 코드 추가 및 수정

--- src/main/java/com/sparta/refrigerator/board/repository/InvitationRepository.java
+++ src/main/java/com/sparta/refrigerator/board/repository/InvitationRepository.java
@@ -9,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvitationRepository extends JpaRepository <Invitation, Long> {
     boolean existsByBoardAndUser(Board board, User user);
-    boolean existsByBoardIdAndUserIdAndUserRole(Long boardId, Long userId, UserAuth auth);
+    boolean existsByBoardIdAndUserIdAndUserRole(Long Board, Long User, UserAuth auth);
 }
