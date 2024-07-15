f83cb75e89ad06941215d348925cdf743351d9de
hoon <165642393+seonghoon90@users.noreply.github.com>
2024-07-16 03:25:01 +0900

    🩹 fix :   CommentRepository 페이지네이션 제거

--- src/main/java/com/sparta/refrigerator/comment/repository/CommentRepository.java
+++ src/main/java/com/sparta/refrigerator/comment/repository/CommentRepository.java
@@ -2,13 +2,13 @@ package com.sparta.refrigerator.comment.repository;
 
 import com.sparta.refrigerator.card.entity.Card;
 import com.sparta.refrigerator.comment.entity.Comment;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    Page<Comment> findAllByCard(Card card, Pageable pageable);
+    List<Comment> findAllByCardOrderByCreatedAtDesc(Card card);
+
 }
